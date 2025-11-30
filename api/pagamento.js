9081ccabac05d7d2a73252524c5da2d2bbab15ed
Luis Davi <74660825+luis0davi@users.noreply.github.com>
2025-11-30 01:01:40 -0300

    Implement payment handler using Mercado Pago

---
+++ api/pagamento.js
@@ -0,0 +1,32 @@
+import mercadopago from "mercadopago";
+
+export default async function handler(req, res) {
+  mercadopago.configure({
+    access_token: process.env.MP_ACCESS_TOKEN
+  });
+
+  const preference = {
+    items: [
+      {
+        id: "1",
+        title: "Produto Teste",
+        quantity: 1,
+        currency_id: "BRL",
+        unit_price: 10
+      }
+    ],
+    back_urls: {
+      success: "https://google.com",
+      failure: "https://google.com",
+      pending: "https://google.com"
+    },
+    auto_return: "approved"
+  };
+
+  try {
+    const response = await mercadopago.preferences.create(preference);
+    res.status(200).json({ link: response.body.init_point });
+  } catch (err) {
+    res.status(500).json({ error: "Falha ao gerar link." });
+  }
+}
